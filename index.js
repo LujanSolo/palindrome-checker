70703818f37dc486a57bea6922702c2affb9cea2
LujanSolo <lujansolo@proton.me>
2023-12-25 20:32:55 -0800

    added 'hidden' class to the results div; began js variable imports

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const textInput = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+// You should have a div element with an id of result
+// When you click on the #check-btn element without entering a value into the #text-input element, an alert should appear with the text Please input a value
+// When the #text-input element only contains the letter A and the #check-btn element is clicked, the #result element should contain the text A is a palindrome
+// When the #text-input element contains the text eye and the #check-btn element is clicked, the #result element should contain the text eye is a palindrome
+// When the #text-input element contains the text _eye and the #check-btn element is clicked, the #result element should contain the text eye is a palindrome
+// When the #text-input element contains the text race car and the #check-btn element is clicked, the #result element should contain the text race car is a palindrome
