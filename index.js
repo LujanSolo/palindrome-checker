13fbafe6c3ec26dfb16c4283ed966f59dd01782b
LujanSolo <lujansolo@proton.me>
2023-12-25 21:56:33 -0800

    built conditional statement with string methods to compare strings

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
@@ -12,7 +11,13 @@ checkBtn.addEventListener('click', (e) => {
     alert('Please input a value');
   } else {
     let cleanText = text.replace(/[^A-Z0-9]/ig, "").toLowerCase();
-    console.log(cleanText);
+    let reverseText = cleanText.split("").reverse().join("");
+    result.classList.remove("hidden");
+    if(cleanText === reverseText){
+      result.innerHTML = `<p>${text} is a palindrome</p>`;
+    } else{
+      result.innerHTML = `<p>${text} is not a palindrome</p>`;
+    }
   }
-})
+});
 //* Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
